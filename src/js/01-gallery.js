189133e2a0709a94d090660bd4080ef72617a397
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2023-11-11 11:07:26 +0100

    add markup

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector("ul");
+list.classList.add("gallery")
+const markup = galleryItems.map(({ preview, original, description }) => `
+<li class="gallery__item">
+<img class="gallery__image" src="${preview}" alt="${description}"/>
+</li>
+`
+    
+).join("")
+list.innerHTML=markup;
