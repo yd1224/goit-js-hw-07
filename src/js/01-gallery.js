863d4c4c9d27e1fffea366ca576d0898792eaa2e
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2023-11-12 17:33:04 +0100

    finish task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,13 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const list = document.querySelector("ul");
-list.classList.add("gallery")
+const container = document.querySelector("ul.gallery");
+
+container.classList.add("gallery")
+
 const markup = galleryItems.map(({ preview, original, description }) => `
 <li class="gallery__item">
-<img class="gallery__image" src="${preview}" alt="${description}"/>
+  <a class="gallery__link" href="${original}" rel="noopener" onclick="return false;" download>
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
 </li>
-`
-    
-).join("")
-list.innerHTML=markup;
+`).join("")
+
+container.innerHTML = markup;
+container.addEventListener("click", handleClick);
+
+
+function handleClick(event) {
+
+    if (event.target === event.currentTarget) {
+        return;
+    }
+    const currentProduct = event.target.closest(".gallery__image");
+    const src = currentProduct.dataset.source;
+    const image = galleryItems.find(({ original }) => original === src);
+    const instance = basicLightbox.create(`
+    <div>
+    <img class="gallery__image" src="${image.original}" alt="${image.description}"/>
+    </div>
+
+`)
+    instance.show()
+
+    document.addEventListener("keydown", handleKey)
+   function handleKey(event) {
+    if (event.code === 'Escape') {
+        instance.close();
+    }
+}
+}
+
+
+
