1c387bba34b93dc9beb3c9e64dbc7b417daed9c9
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2023-11-13 15:00:59 +0100

    init task-02

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector("ul.gallery");
+
+container.classList.add("gallery")
+
+const markup = galleryItems.map(({ preview, original, description }) => `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}" rel="noopener" onclick="return false;" download>
+    <img
+      class="gallery__image lazyload"
+      data-src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`).join("")
+
+container.innerHTML = markup;
