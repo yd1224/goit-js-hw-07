234bcb1e8815fdf90c8542caf292c703fc99b318
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2023-11-14 18:34:32 +0100

    Update 02-lightbox.js

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -18,52 +18,10 @@ const markup = galleryItems.map(({ preview, original, description }) => `
 `).join("")
 
 container.innerHTML = markup;
-container.addEventListener("click", handleClick);
+
+let lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250, overlayOpacity: 0.5 });
 
 
-function handleClick(event) {
 
-    if (event.target === event.currentTarget) {
-        return;
-    }
-    const currentProduct = event.target.closest(".gallery__image");
-  const src = currentProduct.dataset.src;
-  const image = galleryItems.find(({ preview }) => preview === src);
-const instance =  `
-<div class="gallery">
-    <a href="${image.original}">
-    <img src="${image.preview}" alt="${image.description}" title="${image.description}"/>
-    </a>
-</div>
 
-`
-  const lightbox = new SimpleLightbox('.gallery a');
-  lightbox.show();
-  console.log(SimpleLightbox);
 
-    document.addEventListener("keydown", handleKey)
-   function handleKey(event) {
-    if (event.code === 'Escape') {
-        lightbox.close();
-    }
-}
-}
-// function handleClick(event) {
-//     if (event.target === event.currentTarget) {
-//         return;
-//     }
-    
-//     const currentProduct = event.target.closest(".gallery__image");
-//     const src = currentProduct.dataset.src;
-//     const image = galleryItems.find(({ preview }) => preview === src);
-    
-//     const lightbox = new SimpleLightbox(`
-//         <div class="gallery">
-//             <a href="${image.original}">
-//                 <img src="${image.preview}" alt="${image.description}" title="${image.description}"/>
-//             </a>
-//         </div>
-//     `);
-    
-//     lightbox.show();
-// }
